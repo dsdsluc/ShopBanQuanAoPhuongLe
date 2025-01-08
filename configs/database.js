deb290741e23370700e6e79a2278ad8936c0750c
Ha Minh Phuong <phuong.haminh20032004@gmail.com>
2025-01-08 22:08:51 +0700

    update

--- configs/database.js
+++ configs/database.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 module.exports.connect = async ()=>{
     try {
         const mongooseUrl = process.env.MONGO_URI;
-        await mongoose.connect(mongooseUrl);
+        await mongoose.connect(mongooseUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 10000
+          });
         console.log("Connect Success!")
     } catch (error) {
         console.log(error);
